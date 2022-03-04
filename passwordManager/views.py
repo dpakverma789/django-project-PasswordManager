7ef91e793317008f93dc210eb9dfd94ce3ea4e74
Deepak Verma <deepakverma789@gmail.com>
2022-03-04 07:11:26 +0530

    password encryption

--- passwordManager/views.py
+++ passwordManager/views.py
@@ -6,6 +6,46 @@ from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
 
 
+def pass_encrypt(simple_text, encrypted_password='', key='x'):
+    try:
+        for i in simple_text:
+            if i.isalpha():
+                ascii_pass_encrypt = ord(i)
+                binary_pass = hex(ascii_pass_encrypt)
+            elif i.isnumeric():
+                binary_pass = hex(int(i))
+            else:
+                ascii_pass_encrypt = ord(i)
+                binary_pass = hex(ascii_pass_encrypt)
+            encrypted_password += binary_pass[2:] + key
+    except:
+        encrypted_password = simple_text
+    finally:
+        return encrypted_password
+
+
+def pass_decrypt(password_Encrypted, key='x'):
+    splited_password = password_Encrypted.split(key)[:-1]
+    return loopKey(splited_password)
+
+
+def loopKey(splited_password, orignal_password=''):
+    sample = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-']
+    sample2 = ['_', '+', '=', '[', ']', '{', '}', '"', "'"]
+    for x in splited_password:
+        concat_data = int(x, 16)
+        flag = chr(int(concat_data))
+        if flag.isalpha():
+            ascii_pass_decrypt = str(flag)
+            orignal_password += ascii_pass_decrypt
+        elif flag in sample or flag in sample2:
+            ascii_pass_decrypt = str(flag)
+            orignal_password += ascii_pass_decrypt
+        else:
+            orignal_password += str(concat_data)
+    return orignal_password
+
+
 def home(request):
     if request.user.is_authenticated:
         data = {'title': 'PassWord Manager',
@@ -16,8 +56,8 @@ def home(request):
                 cred = Credentials()
                 cred.website = request.POST.get('site')  # fetching site data from request
                 cred.username = request.POST.get('text')
-                cred.password = request.POST.get('password')
                 cred.login_user = request.user
+                cred.password = pass_encrypt(request.POST.get('password'))
                 cred.save()  # saving cred data into database
                 data.update({'flag': 'success',  'msg': 'Credentials Saved!'})
                 return render(request, 'password_manager.html', {'data': data})
@@ -36,8 +76,9 @@ def recovery(request):
             if check_password(request.POST.get('pass'), request.user.password):
                 try:
                     cred = Credentials.objects.get(website=request.POST.get('site'), login_user=request.user)
+                    password_decrypted = pass_decrypt(cred.password)
                     data = {'website': cred.website, 'username': cred.username,
-                            'password': cred.password, 'title': 'PassWord Manager',
+                            'password': password_decrypted, 'title': 'PassWord Manager',
                             'header': 'DASHLINE', 'user': request.user if request.user else 'Guest'}
                 except:
                     data.update({'flag': 'failed', 'msg': 'Not Found!'})
@@ -47,3 +88,4 @@ def recovery(request):
         return render(request, 'recover.html', {'data': data})
     else:
         return redirect('signin-page')
+    
