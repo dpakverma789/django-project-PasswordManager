2626082ceffec8a29e2029a619def2c71d8668f5
Deepak Verma <deepakverma789@gmail.com>
2022-03-04 23:14:40 +0530

    debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8l-6y=#g7y@nyw(pet$&_p@d3itt*^2#bqluli=w*_#%q@qx3h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'managepassword.herokuapp.com']
+ALLOWED_HOSTS = ['managepassword.herokuapp.com']
 
 # Application definition
 
