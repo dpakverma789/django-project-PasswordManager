d45b610fc202708cce91a4e0b0de2935f86f27b5
Deepak Verma <deepakverma789@gmail.com>
2022-03-05 18:09:16 +0530

    bugs fix

--- passwordManager/views.py
+++ passwordManager/views.py
@@ -39,13 +39,13 @@ def recovery(request):
                         data = {'website': cred.website, 'username': cred.username,
                                 'password': password_decrypted, 'title': 'PassWord Manager',
                                 'header': 'DASHLINE', 'user': request.user}
+                        return render(request, 'password_manager.html', {'data': data})
                     else:
                         cred.delete()
                         data.update({'color': '#9933ff', 'msg': 'Credentials Removed!'})
                 except:
                     data.update({'msg': 'Website not Found!', 'color': '#ff3333'})
                     return render(request, 'recover.html', {'data': data})
-                return render(request, 'password_manager.html', {'data': data})
             else:
                 messages.error(request, 'Your Password is Incorrect!')
         return render(request, 'recover.html', {'data': data})
@@ -55,6 +55,7 @@ def recovery(request):
 
 def update(request):
     if request.user.is_authenticated:
+        is_done = False
         data = {'title': 'PassWord Update', 'header': 'UPDATE', 'user': request.user}
         if request.POST and request.method == 'POST':
             if check_password(request.POST.get('pass'), request.user.password):
@@ -62,12 +63,20 @@ def update(request):
                     cred = Credentials.objects.get(website=request.POST.get('site'), login_user=request.user)
                     if request.POST.get('update') == 'website':
                         cred.website = request.POST.get('text')
+                        is_done = True
                     if request.POST.get('update') == 'username':
                         cred.username = request.POST.get('text')
+                        is_done = True
                     if request.POST.get('update') == 'password':
                         cred.password = pass_encrypt(request.POST.get('text'))
-                    cred.save()
-                    data.update({'color': '#47d147', 'msg': f"{request.POST.get('update')} has been updated!"})
+                        is_done = True
+                    if is_done:
+                        cred.save()
+                        color = '#47d147'
+                        msg = f"{request.POST.get('update')} has been updated!"
+                    else:
+                        color = '#ff3333'
+                    data.update({'color': color, 'msg': request.POST.get('update')})
                 except:
                     data.update({'msg': 'Website not Found!', 'color': '#ff3333'})
                 return render(request, 'update.html', {'data': data})
