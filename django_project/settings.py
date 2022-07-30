9765b43776104e25c7deb6523ce16d3e8985f5be
deepakv-ksolves <deepak.verma@ksolves.com>
2022-07-30 21:34:11 +0530

    - updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,6 +140,8 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INACTIVITY_TIME = 300
 SESSION_COOKIE_AGE = INACTIVITY_TIME
