ef72d4184535bc3cd153b4b5e286b12b2add525a
deepakv-ksolves <deepak.verma@ksolves.com>
2022-09-06 15:11:27 +0530

    - notification

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '#'.join(GET_FULL_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.251.76', '127.0.0.1', '192.168.248.35']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
