13a08fcc24d22435b43c8c95b51a0b22b1436701
deepakv-ksolves <deepak.verma@ksolves.com>
2022-09-10 19:11:35 +0530

    new security update

--- passwordManager/password_ecryptor.py
+++ passwordManager/password_ecryptor.py
@@ -1,30 +1,32 @@
-# new key q5e2c
-def pass_encrypt(simple_text, encrypted_password='', key='x'):
+
+
+def pass_encrypt(simple_text, encrypted_password='', key='ec2'):
     try:
         for i in simple_text:
             if i.isalpha():
                 ascii_pass_encrypt = ord(i)
-                binary_pass = hex(ascii_pass_encrypt)
+                hexa_pass = hex(ascii_pass_encrypt)
             elif i.isnumeric():
-                binary_pass = hex(int(i))
+                hexa_pass = hex(int(i))
             else:
                 ascii_pass_encrypt = ord(i)
-                binary_pass = hex(ascii_pass_encrypt)
-            encrypted_password += binary_pass[2:] + key
+                hexa_pass = hex(ascii_pass_encrypt)
+            encrypted_password += hexa_pass[2:] + key
     except:
         encrypted_password = simple_text
     finally:
         return encrypted_password
 
 
-def pass_decrypt(password_Encrypted, key='x'):
+def pass_decrypt(password_Encrypted):
+    key = password_Encrypted[-3:]
     splited_password = password_Encrypted.split(key)[:-1]
-    return loopKey(splited_password,password_Encrypted)
+    return loopKey(splited_password, password_Encrypted)
 
 
 def loopKey(splited_password, password_Encrypted, orignal_password=''):
     sample = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-']
-    sample2 = ['_', '+', '=', ' ', '[', ']', '{', '}', '"', "'"]
+    sample2 = ['_', '+', '=', '[', ']', '{', '}', '"', "'", ' ']
     is_done = False
     for x in splited_password:
         concat_data = int(x, 16)
--- passwordManager/static/js/password_manager.js
+++ passwordManager/static/js/password_manager.js
@@ -1,7 +1,18 @@
+
 	function toggle_check(event)
 	{
 	    let flag_check = event.currentTarget.checked;
         let dash_check = event.currentTarget.previousElementSibling;
         let field = dash_check != null ? dash_check : event.currentTarget.parentElement.previousElementSibling;
         field.type = flag_check === true ? "text" : "password";
+	}
+
+	function checkPasswordLength()
+	{
+	   let password = document.getElementsByClassName('pass');
+	   password_length = password[0].value.length;
+	   if (password_length > 20){
+	        alert('Max 20 character Password is allowed!');
+	        return false
+	   }
 	}
