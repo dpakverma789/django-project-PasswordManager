8ace7827d301a67e0f5651f49a693b497ac618a5
Deepak Verma <deepakverma789@gmail.com>
2023-04-02 11:20:58 +0530

    db config

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 MEDIA_URL = '/media/'
