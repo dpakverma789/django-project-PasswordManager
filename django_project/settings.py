fabf8d14aad598ea28eaf6e55ca7bbe0b054dfa0
Deepak Verma <dpakverma789@gmail.com>
2024-07-03 12:16:35 +0530

    minor fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,18 +38,28 @@ INSTALLED_APPS = [
     'passwordManager',
     'users',
     'import_export',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django_session_timeout.middleware.SessionTimeoutMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://managerpass.vercel.app'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://managerpass.vercel.app'
 ]
 
 ROOT_URLCONF = 'django_project.urls'
