7bd93a06d0873782b142331c9a423744409b3a49
deepak verma <deepak.verma@logicloop.io>
2024-10-07 11:59:41 +0530

    minor fix

--- build_files.sh
+++ build_files.sh
@@ -1,26 +1,8 @@
-#!/bin/bash
-
-# Install pip if not available
-if ! command -v pip &> /dev/null
-then
-    echo "pip not found, installing..."
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py --user
-fi
-
-# Activate virtual environment (if necessary)
-python -m venv venv
-source venv/bin/activate
-
 echo "======> INSTALLING REQUIREMENTS <======"
 pip install -r requirements.txt
 echo "======> REQUIREMENTS INSTALLED <======"
 
 echo "======> COLLECTING STATIC FILES <======"
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 echo "======> STATIC FILES COLLECTED <======"
 
-echo "======> MAKE-MIGRATIONS <======"
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-echo "======> MAKE-MIGRATIONS-END <======"
