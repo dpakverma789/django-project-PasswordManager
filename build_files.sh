7e8ce5f35f897ff48aa1543ad4a924c4f6d5052f
deepak verma <deepak.verma@logicloop.io>
2024-10-07 12:03:25 +0530

    minor fix

--- build_files.sh
+++ build_files.sh
@@ -6,3 +6,7 @@ echo "======> COLLECTING STATIC FILES <======"
 python3 manage.py collectstatic --noinput
 echo "======> STATIC FILES COLLECTED <======"
 
+echo "======> MAKE-MIGRATIONS <======"
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+echo "======> MAKE-MIGRATIONS-END <======"
